415e219b95ccd78678f7667d5f1b89eea8a58df1
Preyash Patel <preyashpatel97@gmail.com>
2024-01-18 21:36:26 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter a number: ");
+//        double number = scanner.nextDouble();
+//        scanner.close();
+//        if (number > 0) {
+//            System.out.println("The number is positive.");
+//        } else if (number < 0) {
+//            System.out.println("The number is negative.");
+//        } else {
+//            System.out.println("The number is zero.");
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int number = scanner.nextInt();
+//        scanner.close();
+//        if (number % 2 == 0) {
+//            System.out.println("The number is even.");
+//        } else {
+//            System.out.println("The number is odd.");
+//        }
+
+        
+    }
+}
